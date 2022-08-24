3f5d3ea5ae649b624f4f72e5a524c686a802abd2
abdallah monastir <sidibeabdallah01@gmail.com>
2022-08-24 15:40:53 +0200

    second commit

--- models/Post.php
+++ models/Post.php
@@ -1,5 +1,6 @@
 <?php
-class Post{
+class Post
+{
     //DB Stuff
     private $conn;
     private $table = 'posts';
@@ -14,12 +15,14 @@ class Post{
     public $created_at;
 
     //Constructor with DB
-    public function __construct($db){
-        $this->conn = $db;      
+    public function __construct($db)
+    {
+        $this->conn = $db;
     }
 
     //Get Posts
-    public function read(){
+    public function read()
+    {
         //create query
         $query = 'SELECT 
              c.name as category_name,
@@ -30,7 +33,7 @@ class Post{
              p.author,
              p.created_at
              FROM
-             ' .$this->table. ' p
+             ' . $this->table . ' p
              LEFT JOIN
                categories c ON p.category_id = c.id
                ORDER BY
@@ -42,5 +45,84 @@ class Post{
         //Execute query
         $stmt->execute();
         return $stmt;
+    }
+
+    // Get Single Post
+    public function read_single()
+    {
+        //create query
+        $query = 'SELECT 
+             c.name as category_name,
+             p.id,
+             p.category_id,
+             p.title,
+             p.body,
+             p.author,
+             p.created_at
+             FROM
+             ' . $this->table . ' p
+             LEFT JOIN
+               categories c ON p.category_id = c.id
+             WHERE 
+             p.id = ?
+             LIMIT 0,1';
+
+        //Prepare statement
+        $stmt = $this->conn->prepare($query);
+
+        //Bind ID
+        $stmt->bindParam(1, $this->id);
+
+        //Execute query
+        $stmt->execute();
+        /**
+         * La fonction fetch_assoc() / mysqli_fetch_assoc()
+         *  est une fonction intégrée en PHP qui récupère 
+         * une ligne de résultat sous forme de tableau associatif.
+         * 
+         * Valeur de retour
+         * La fonction renvoie un tableau associatif de chaînes
+         *  de caractères représentant la ligne récupérée. 
+         * NULL s’il n’y a plus de lignes dans le jeu de résultats.
+         */
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        //Set properties
+        $this->title = $row['title'];
+        $this->body = $row['body'];
+        $this->author = $row['author'];
+        $this->category_id = $row['category_id'];
+        $this->category_name = $row['category_name'];
+    }
+
+    //Create Post
+    public function create()
+    {
+        //Create query
+        $query = 'INSERT INTO ' . $this->table . ' SET title = :title, body = :body, author = :author, category_id = :category_id';
+
+        //Prepare statement
+        $stmt = $this->conn->prepare($query);
+
+        //Clean data
+        $this->title = htmlspecialchars(strip_tags($this->title));
+        $this->body = htmlspecialchars(strip_tags($this->body));
+        $this->author = htmlspecialchars(strip_tags($this->author));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+
+        //Bind data
+        $stmt->bindParam(':title', $this->title);
+        $stmt->bindParam(':body', $this->body);
+        $stmt->bindParam(':author', $this->author);
+        $stmt->bindParam(':category_id', $this->category_id);
+
+        //Execute query
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        // Print error if something goes wrong
+        printf("Error: %s. \n", $stmt->error);
+        return false;
     }
 }
